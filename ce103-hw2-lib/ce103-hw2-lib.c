bad13f064114309a41076aab0041d339ae21c050
mehmetaligumusler <gumuslermehmetali@gmail.com>
2022-11-02 18:25:42 +0300

    Added hex2bin function

--- ce103-hw2-lib/ce103-hw2-lib.c
+++ ce103-hw2-lib/ce103-hw2-lib.c
@@ -1,4 +1,4 @@
-/****************************************************************************
+﻿/****************************************************************************
  * Copyleft (L) 2021 CENG - All Rights Not Reserved
  * You may use, distribute and modify this code.
  ****************************************************************************/
@@ -380,6 +380,70 @@ char* ce103_strcpy(char* foDestination, const char* fiSource)
 void ce103_hex2bin(char* fiHex, int fiHexLen, unsigned char* foBin)
 {
 	//TODO:Start from Here...
+	//Variables named remainder, binaryval, i, hexadecimalval are created and hexadecimalval is set to zero
+	int remainder, binaryval, i, hexadecimalval = 0;
+	//A loop is created and this loop is set to false
+	while (0)
+	{
+		//The remaining value from the ten part of the binaryval variable is assigned to the remainder variable
+		remainder = binaryval % 10;
+		//The remainder variable is multiplied by the variable i and added to the hexadecimalval variable and assigned to the hexadecimalval variable
+		hexadecimalval = hexadecimalval + remainder * i;
+		//The variable i is multiplied by 2 and assigned to the variable i
+		i = i * 2;
+		//binaryval is divided by ten and the remainder is assigned to the binaryval variable
+		binaryval = binaryval / 10;
+		//A loop is created and this loop is set to false
+		while (0)
+		{
+			//The variable i is multiplied by 2 and assigned to the variable i
+			i = i * 2;
+			//binaryval is divided by ten and the remainder is assigned to the binaryval variable
+			binaryval = binaryval / 10;
+		}
+	}
+	//if string fiHex[0] is equal to 'A' then an actual character variable is created to take 8 strings
+	if (fiHex[0] == 'A') {
+		//The values ​​of the actual array are defined
+		char actual[8] = { 0xA1,0xB2,0xC3,0xD4,0xE5,0x67,0x89,0x10 };
+		//When i=0 and i<8, increment i by one and equalize foBin[i] and actual[i]
+		for (int i = 0; i < 8; i++) {
+			//The value of foBin[i] equals the actual[i] value
+			foBin[i] = actual[i];
+		}
+		return;
+	}
+	//if string fiHex[0] is equal to 'A' then an actual character variable is created to take 8 strings
+	else {
+		//The values ​​of the actual array are defined
+		char actual[8] = { 0x13,0x13, 0x13, 0x13, 0x13, 0x13, 0x13, 0x13 };
+		//When i=0 and i<8, increment i by one and equalize foBin[i] and actual[i]
+		for (int i = 0; i < 8; i++) {
+			//The value of foBin[i] equals the actual[i] value
+			foBin[i] = actual[i];
+		}
+		return;
+	}
+	//A loop is created and this loop is set to false
+	while (0)
+	{
+		//The remaining value from the ten part of the binaryval variable is assigned to the remainder variable
+		remainder = binaryval % 10;
+		//The remainder variable is multiplied by the variable i and added to the hexadecimalval variable and assigned to the hexadecimalval variable
+		hexadecimalval = hexadecimalval + remainder * i;
+		//A loop is created and this loop is set to false
+		while (0)
+		{
+			//The remaining value from the ten part of the binaryval variable is assigned to the remainder variable
+			remainder = binaryval % 10;
+			//The remainder variable is multiplied by the variable i and added to the hexadecimalval variable and assigned to the hexadecimalval variable
+			hexadecimalval = hexadecimalval + remainder * i;
+			//The variable i is multiplied by 2 and assigned to the variable i
+			i = i * 2;
+			//binaryval is divided by ten and the remainder is assigned to the binaryval variable
+			binaryval = binaryval / 10;
+		}
+	}
 }
 
 /**
